929e0744ba7325a5000d22b4a6afc390da3b59e2
José Wilson <165939651+willokkoifpb@users.noreply.github.com>
2025-07-30 08:17:35 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,6 @@ import javax.swing.SwingUtilities;
 public class Main {
     public static void main(String[] args) {
         // Garante que a interface gráfica seja criada na thread de eventos do Swing (Event Dispatch Thread - EDT)
-        // Esta é a forma correta e mais segura de iniciar aplicações Swing.
         SwingUtilities.invokeLater(() -> {
             TelaPrincipal tela = new TelaPrincipal();
             tela.setVisible(true);
